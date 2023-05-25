6d1ecf2aea536c6fed318d69934bf02988937d07
Thomas Bley <mails@thomasbley.de>
2023-05-25 17:51:57 +0200

    testing

--- src/test.php
+++ src/test.php
@@ -3,7 +3,7 @@
 try {
     $pdo = new \PDO('mysql:host=127.0.0.1;dbname=tasks;port=3306;charset=utf8mb4;', 'root', 'root');
     $ids = implode(',', array_map(intval(...), [1,2,3]));
-    $query = sprintf('SELECT id, title, duedate, completed, last_updated_by FROM task_invalid WHERE id IN (%s)', $ids);
+    $query = sprintf('SELECT id, title, duedate, completed, last_updated_by FROM task WHERE id IN (%s)', $ids);
     $statement = $pdo->query($query);
     print_r($statement->fetchAll());
 }
@@ -19,28 +19,38 @@ catch (Exception $e) { echo $e->__toString(); }
 
 try {
     $pdo = new \PDO('mysql:host=127.0.0.1;dbname=tasks;port=3306;charset=utf8mb4;', 'root', 'root');
-    $query = 'SELECT id, title, duedate, completed, last_updated_by FROM tasks_invalid WHERE customer_id = ? AND completed = 1';
+    $query = 'SELECT id, title, duedate, completed, last_updated_by FROM task_invalid WHERE customer_id = ? AND completed = 1';
     $statement = $pdo->prepare($query);
     $statement->execute([123]);
-    $result = $statement->fetch();
+    print_r($statement->fetchAll());
 }
 catch (Exception $e) { echo $e->__toString(); }
 
 try {
     $pdo = new \PDO('mysql:host=127.0.0.1;dbname=tasks;port=3306;charset=utf8mb4;', 'root', 'root');
-    $query = 'SELECT id, title, duedate, completed, last_updated_by FROM tasks_invalid WHERE customer_id = 123 AND completed = 1';
+    $query = 'SELECT id, title, duedate, completed, last_updated_by FROM task_invalid WHERE customer_id = 123 AND completed = 1';
     $statement = $pdo->prepare($query);
     $statement->execute([]);
-    $result = $statement->fetch();
+    print_r($statement->fetchAll());
 }
 catch (Exception $e) { echo $e->__toString(); }
 
 try {
     $pdo = new \PDO('mysql:host=127.0.0.1;dbname=tasks;port=3306;charset=utf8mb4;', 'root', 'root');
-    $query = 'SELECT id, title, duedate, completed, last_updated_by FROM tasks_invalid WHERE customer_id = ? AND completed = 1';
+    $query = 'SELECT id, title, duedate, completed, last_updated_by FROM task_invalid WHERE customer_id = ? AND completed = 1';
     $statement = $pdo->prepare($query);
     $statement->bindValue(1, 123, PDO::PARAM_INT);
     $statement->execute();
-    $result = $statement->fetch();
+    print_r($statement->fetchAll());
+}
+catch (Exception $e) { echo $e->__toString(); }
+
+try {
+    $pdo = new \PDO('mysql:host=127.0.0.1;dbname=tasks;port=3306;charset=utf8mb4;', 'root', 'root');
+    $query = 'SELECT id, title, duedate, completed, last_updated_by FROM task_invalid WHERE customer_id = :id AND completed = 1';
+    $statement = $pdo->prepare($query);
+    $statement->bindValue(':id', 123, PDO::PARAM_INT);
+    $statement->execute();
+    print_r($statement->fetchAll());
 }
 catch (Exception $e) { echo $e->__toString(); }
